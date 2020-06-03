12e93425c7e933ba127487a34b5b4fa0f07cdd83
dropdel <derrickethornton@protonmail.com>
2020-06-03 04:29:31 +0100

    get notes and add note functions added to notes.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const validator = require('validator');
 const chalk = require('chalk');
-const getNotes = require('./notes')
+const getNotes = require('./notes');
 
 // add note, remove note, read note, list note
---
+++ notes.js
@@ -0,0 +1,12 @@
+function getNotes() {
+    return "Your notes..."
+}
+
+const addNote = function(title, body) {
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
