a7301a076e0bacd51502bbd1a29e54274c7fd1ec
dropdel <derrickethornton@protonmail.com>
2020-06-03 05:33:21 +0100

    Add command added to app.js and notes.js json for add/storage of notes

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
+
+// Add Command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    building: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        getNotes.addNote(argv.title, argv.body);
+    }
+})
+
 // add note, remove note, read note, list note
+// you will add an argument and option at the command line...
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,9 +1,40 @@
+// using the file system to get json of list
+const fs = require('fs');
+
 function getNotes() {
     return "Your notes..."
 }
 
 const addNote = function(title, body) {
+    // Loading notes
+    const notes = loadNotes();
 
+    // Adding to note
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    // Save notes and rewrite json
+    savedNotes(notes);
+}
+
+const savedNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes);
+
+    // Rewrite json file
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    }
+    catch(e) {
+        return [];
+    }
 }
 
 module.exports = {
